cf90a98950dc457ccc26a8394f7b9075910f841a
Mateus Werneck <werneck.mateus@gmail.com>
2021-04-10 09:42:10 -0300

    Update decoding.py

--- decoding.py
+++ decoding.py
@@ -2,7 +2,7 @@ import re
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from twitter import getTwitter
-from instagram import instaPost, instaStories
+from instagram import instaPost, instaStories, instaTV
 from youtube import getYou
 ##################################### Open Firefox #########################################
 firefox_options = Options()
@@ -10,11 +10,13 @@ firefox_options.add_argument("--headless")
 firefox = webdriver.Firefox(options=firefox_options)
 firefox.set_page_load_timeout(60)
 #################################### Process Media Type and Get Media #######################
-def getMedia(url, button):
+def getMedia(url, button, bot, chat_id):
      url = url
+     igtv = re.search('/tv/', url)
      stories = re.search('/stories/', url)
      posts = re.search('/p/', url)
      reel = re.search ('/reel/', url)
+     igtv = re.search('/tv/', url)
      twt = re.search('twitter.com/', url)
      twt2 = re.search('t.co', url)
      yt = re.search('youtube', url)
@@ -22,7 +24,7 @@ def getMedia(url, button):
 ################### Posts and Reels Instagram #######################################
 
      if (posts != None or reel != None):
-        source = instaPost(url, firefox, button)
+        source = instaPost(url, firefox, button, bot, chat_id)
         if (re.search('instagram', source) != None):
           return source
         else:
@@ -32,9 +34,23 @@ def getMedia(url, button):
 ################## Instagram Stories ################################################
 
      elif(stories != None):
-        source = instaStories(url, firefox, button)
+        source = instaStories(url, firefox, button, bot, chat_id)
         if (re.search('instagram', source) != None):
             return source
+        else:
+            source = ''
+            return source
+
+#################### IGTV ###############################################################
+     elif(igtv != None):
+        source = instaTV(url, firefox, button, bot, chat_id)
+        if type(source) is list:
+            for i in source:
+                if (re.search('IGTV', i) != None):
+                    return source
+        elif type(source) is str:
+            if re.search('.mp4', source) != None:
+                return source
         else:
             source = ''
             return source
@@ -43,12 +59,12 @@ def getMedia(url, button):
 
      elif twt != None or twt2 != None:
         path = ''
-        path = getTwitter(url, firefox)
+        path = getTwitter(url, firefox, bot, chat_id)
         return path
 
 ################ Youtube Audio ##########################################################
 
      elif yt != None or yt2 != None:
         path = ''
-        path = getYou(url)
+        path = getYou(url, bot, chat_id)
         return path
