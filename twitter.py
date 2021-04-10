1929858f95c55f500958a925be4a96b2259f5687
Mateus Werneck <werneck.mateus@gmail.com>
2021-04-10 09:41:40 -0300

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,23 +1,23 @@
-import requests, re, time, urllib, os, json, tempfile, shutil
+import requests, re, time, urllib, os, webbrowser, json, tempfile, shutil
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from natsort import natsorted
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 
 ####################################### Twitter Video #####################################################
-def getTwitter(url, firefox):
+def getTwitter(url, firefox, bot, chat_id):
     temp_dir = tempfile.mkdtemp()
-    directory_final = 'C:\\Users\\Mateus\\Documents\\GitHub\\telegram_bot'
+    directory_final = '/home/mateus/Documents/Python/telegram_bot'
     firefox.get(url)
     time.sleep(5)
     log = firefox.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
     video =''
-    with open(temp_dir + "\\items.json", "w") as jsonFile:
+    path = ''
+    with open(temp_dir + "/items.json", "w") as jsonFile:
         json.dump(log, jsonFile, indent = 6)
     jsonFile.close()
-    with open (temp_dir + "\\items.json", "r") as jsonFile:
+    with open (temp_dir + "/items.json", "r") as jsonFile:
         json_decoded = json.load(jsonFile)
         for i in range(len(json_decoded)):
             if (re.match(r'https://video.twimg.com/', json_decoded[i]['name']) != None):
@@ -29,8 +29,8 @@ def getTwitter(url, firefox):
         encoded = []
         if (m3u8c == None):
             tipo='m4s'
-            urllib.request.urlretrieve(video, temp_dir + '\\temp.txt')
-            with open (temp_dir + "\\temp.txt", "r") as tempF:
+            urllib.request.urlretrieve(video, temp_dir + '/temp.txt')
+            with open (temp_dir + "/temp.txt", "r") as tempF:
                 for line in tempF:
                     if(re.search(r'.ts', line) != None):
                         encoded.append('https://video.twimg.com' + line)
@@ -39,31 +39,31 @@ def getTwitter(url, firefox):
             collection =[]
             if(tipo == 'ts'):
                 for i in range(len(encoded)):
-                    name = temp_dir + '\\temp' + '0' + str(i) + '.ts'
+                    name = temp_dir + '/temp' + '0' + str(i) + '.ts'
                     urllib.request.urlretrieve(encoded[i], name)
                     video = VideoFileClip(name)
                     collection.append(video)
+                bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
                 twitter = concatenate_videoclips(collection)
                 final_name = 'twitter.mp4'
-                twitter.to_videofile(final_name, threads=8, preset="veryfast", audio_codec='aac')
-                shutil.rmtree(temp_dir)
-                path = (directory_final + '\\' + final_name)
+                twitter.to_videofile(directory_final + '/' + final_name, threads=8, logger=None, rewrite_audio=False, audio_codec='aac', preset = 'veryfast', fps=24)
+                path = (directory_final + '/' + final_name)
                 return path
             else:
-                path = ''
+                path = 'inválido'
                 return path
 
         elif (m3u8c != None):
             tipo='m4s'
-            urllib.request.urlretrieve(video, temp_dir + '\\temp.txt')
-            with open (temp_dir + "\\temp.txt", "r") as tempF:
+            urllib.request.urlretrieve(video, temp_dir + '/temp.txt')
+            with open (temp_dir + "/temp.txt", "r") as tempF:
                 for line in tempF:
                     if(re.search(r'.m3u8', line) != None):
                         m3u8c2 = 'https://video.twimg.com' + line
                         break
             tempF.close()
-            urllib.request.urlretrieve(m3u8c2, temp_dir + '\\temp.txt')
-            with open (temp_dir + "\\temp.txt", "r") as tempF:
+            urllib.request.urlretrieve(m3u8c2, temp_dir + '/temp.txt')
+            with open (temp_dir + "/temp.txt", "r") as tempF:
                 for line in tempF:
                     if(re.search(r'.ts', line) != None):
                         encoded.append('https://video.twimg.com' + line)
@@ -72,24 +72,24 @@ def getTwitter(url, firefox):
             collection =[]
             if (tipo == 'ts'):
                 for i in range(len(encoded)):
-                    name = temp_dir + '\\temp' + '0' + str(i) + '.ts'
+                    name = temp_dir + '/temp' + '0' + str(i) + '.ts'
                     urllib.request.urlretrieve(encoded[i], name)
                     video = VideoFileClip(name)
                     collection.append(video)
+                bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
                 twitter = concatenate_videoclips(collection)
                 final_name = 'twitter.mp4'
-                twitter.to_videofile(final_name, threads=8, preset="veryfast", audio_codec='aac')
-                shutil.rmtree(temp_dir)
-                path = (directory_final + '\\' + final_name)
+                twitter.to_videofile(directory_final + '/' + final_name, threads=8, logger=None, rewrite_audio=False, audio_codec='aac', preset = 'veryfast', fps=24)
+                path = (directory_final + '/' + final_name)
                 return path
             else:
-                path = ''
+                path = 'inválido'
                 return path
 
         else:
             tipo='m4s'
             encoded = []
-            with open (temp_dir + "\\items.json", "r") as jsonFile:
+            with open (temp_dir + "/items.json", "r") as jsonFile:
                 json_decoded = json.load(jsonFile)
                 for i in range(len(json_decoded)):
                     if(re.search('"(.*?).ts', json_decoded[i]['name']) != None):
@@ -99,17 +99,17 @@ def getTwitter(url, firefox):
             collection =[]
             if (tipo == 'ts'):
                 for i in range(len(encoded)):
-                    name = temp_dir + '\\temp' + '0' + str(i) + '.ts'
+                    name = temp_dir + '/temp' + '0' + str(i) + '.ts'
                     urllib.request.urlretrieve(encoded[i], name)
                     video = VideoFileClip(name)
                     collection.append(video)
+                bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
                 twitter = concatenate_videoclips(collection)
                 final_name = 'twitter.mp4'
-                twitter.to_videofile(final_name, threads=8, preset="veryfast", audio_codec='aac')
-                shutil.rmtree(temp_dir)
-                path = directory_final + '\\' + final_name
+                twitter.to_videofile(directory_final + '/' + final_name, threads=8, logger=None, rewrite_audio=False, audio_codec='aac', preset = 'veryfast', fps=24)
+                path = directory_final + '/' + final_name
                 return path
             else:
-                path = ''
-                return path            
+                path = 'inválido'
+                return path
 ####################################################################################################
