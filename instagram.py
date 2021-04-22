0dad19aeb0d3ddf853ce571ddc7d1341efa7b928
Mateus Werneck <werneck.mateus@gmail.com>
2021-04-22 09:26:26 -0300

    Better Stories and IGTV processing

--- instagram.py
+++ instagram.py
@@ -1,4 +1,4 @@
-import requests, re, time, json, os, urllib, glob
+import requests, re, time, json, os, urllib, glob, shutil, tempfile
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,48 +6,52 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 
-load = u' \U0001f504'
+load = u'\U0001f504'
+runner = u'\U0001f3c3'
 ################################# Instagram Posts and Reels #########################################
 def instaPost(url, firefox, button, bot, chat_id):
+    bot.send_message(chat_id=chat_id, text='Só um segundo' + runner, timeout=60)
     if (auth(url, firefox, button, bot) == True):
         firefox.get(url)
-        time.sleep(1)
-        '''
-        IGTV = firefox.find_elements_by_xpath('.//span[@class = "PJXu4"]')
-        if (IGTV[1].text == 'IGTV'):
-            instaTV(url, firefox, button)'''
-        #else:
         html = firefox.page_source
-        time.sleep(1)
-        encodedImg = re.search(r'"display_url":"(.*?)",', html)
-        encodedVideo = re.search(r'"video_url":"(.*?)",', html)
-        if (encodedImg == None and encodedVideo == None):
-            decoded = ''
-            return decoded
-        else:
-            bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
-            if (encodedVideo == None):
-                decoded = (encodedImg.group(1).replace(r"\u0026", "&"))
+        try:
+            if firefox.find_element_by_class_name("tWeCl"):
+                video = firefox.find_element_by_class_name("tWeCl").get_attribute("src")
+                if re.search("blob", video) == None:
+                    button += 1
+                    source = instaTV(url, firefox, button, bot, chat_id)
+                    return source
+                else:
+                    encodedVideo = re.search(r'"video_url":"(.*?)",', html)
+                    decoded = (encodedVideo.group(1).replace(r"\u0026", "&"))
+                    return decoded
+        except:
+            encodedImg = re.search(r'"display_url":"(.*?)",', html)
+            if encodedImg == None:
+                decoded = ''
+                return decoded
             else:
-                decoded = (encodedVideo.group(1).replace(r"\u0026", "&"))
-            return decoded
+                decoded = (encodedImg.group(1).replace(r"\u0026", "&"))
+                return decoded
 ############################### Instagram Stories ########################################################
 def instaStories(url, firefox, button, bot, chat_id):
+    bot.send_message(chat_id=chat_id, text='Só um segundo' + runner, timeout=60)
     if (auth(url, firefox, button, bot) == True):
         firefox.get(url)
-        time.sleep(5)
-        view = firefox.find_element_by_class_name('sqdOP.L3NKy.y1rQx.cB_4K')
-        view.click()
+        time.sleep(2)
+        try:
+            view = firefox.find_element_by_class_name('sqdOP.L3NKy.y1rQx.cB_4K')
+            view.click()
+        except:
+            print("No Button Stories")
         time.sleep(1)
         img = ''
         video = ''
         if (bool (len(firefox.find_elements_by_tag_name('source'))) > 0):
             video = firefox.find_element_by_tag_name('source').get_attribute("src")
-            bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
             return video
         elif (bool (len(firefox.find_elements_by_tag_name('source'))) == 0):
             img = firefox.find_element_by_tag_name("img").get_attribute("src")
-            bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
             return img
         else:
             source = ''
@@ -55,6 +59,7 @@ def instaStories(url, firefox, button, bot, chat_id):
 
 def instaTV(url, firefox, button, bot, chat_id):
     bot.send_message(chat_id=chat_id, text='Isso pode demorar um pouco', timeout=60)
+    temp_dir = tempfile.mkdtemp()
     if (auth(url, firefox, button, bot) == True):
         firefox.get(url)
         time.sleep(1)
@@ -65,65 +70,70 @@ def instaTV(url, firefox, button, bot, chat_id):
             decoded = ''
             return decoded
         else:
-            if os.path.isdir('/home/mateus/Documents/Python/telegram_bot/IGTV') == False:
-                os.mkdir('/home/mateus/Documents/Python/telegram_bot/IGTV')
             if (encodedVideo != None):
-                path = '/home/mateus/Documents/Python/telegram_bot/IGTV/IGTV.mp4'
+                path = temp_dir + '/IGTV.mp4'
                 decoded = (encodedVideo.group(1).replace(r"\u0026", "&"))
                 urllib.request.urlretrieve(decoded, path)
                 size = os.path.getsize(path) / 1000000
                 if size <= 49.9999:
+                    bot.send_video(chat_id=chat_id, video = open(path, 'rb'), timeout=3600)
+                    shutil.rmtree(temp_dir)
                     return path
                 else:
                     video = VideoFileClip(path)
                     duration = int(video.duration)
                     start_time = 0
-                    end_time = 360
-                    parts = int(duration / 360)
-                    videopart = int(duration/360) + 1
-                    end_custom = (360 * parts) + (duration % 360)
+                    end_time = 600
+                    parts = int(duration / 600)
+                    videopart = int(duration/600) + 1
+                    end_custom = (600 * parts) + (duration % 600)
                     igtvPath = []
-                    if (parts > (video.duration / 360)):
+                    if (parts > (video.duration / 600)):
                         parts = parts - 1
-                    if (duration <= 390):
-                        bot.send_message(chat_id=chat_id, text='Iniciando conversão...', timeout=60)
+                    if (duration <= 630):
+                        bot.send_message(chat_id=chat_id, text='Finalizando ' + runner, timeout=60)
                         subvideo = video.subclip(0, duration)
-                        subvideo.write_videofile('/home/mateus/Documents/Python/telegram_bot/IGTV/IGTV'  + '01' + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=24, bitrate='860k')
-                        bot.send_message(chat_id=chat_id, text='Finalizando...', timeout=60)
-                        os.remove(path)
-                        igtvPath = glob.glob('/home/mateus/Documents/Python/telegram_bot/IGTV' + '/*.mp4')
+                        subvideo.write_videofile(temp_dir + '/IGTV'  + '01' + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=30, bitrate='520k')
+                        time.sleep(1)
+                        bot.send_video(chat_id=chat_id, video = open(temp_dir + '/IGTV'  + '01' + '.mp4', 'rb'), timeout=3600)
+                        igtvPath = temp_dir + '/IGTV01.mp4'
+                        shutil.rmtree(temp_dir)
                         return igtvPath
                     else:
-                        if (duration % 360 == 0):
-                            bot.send_message(chat_id=chat_id, text='Iniciando conversão...', timeout=60)
+                        if (duration % 600 == 0):
                             c = 1
                             for i in range (parts):
                                 if(end_time > duration):
                                     break
                                 subvideo = video.subclip(start_time, end_time)
                                 bot.send_message(chat_id=chat_id, text='Processando ' + 'parte ' + str(c) + '/' + str(videopart) + '' + load, timeout=60)
-                                subvideo.write_videofile('/home/mateus/Documents/Python/telegram_bot/IGTV/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=24, bitrate='860k')
+                                subvideo.write_videofile(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=30, bitrate='550k')
+                                time.sleep(1)
+                                bot.send_video(chat_id=chat_id, video = open(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', 'rb'), timeout=3600)
                                 c +=1
-                                start_time += 360.00
-                                end_time += 360.00
-                        elif (duration % 360 > 0):
-                            bot.send_message(chat_id=chat_id, text='Iniciando conversão...', timeout=60)
+                                start_time += 600.00
+                                end_time += 600.00
+                        elif (duration % 600 > 0):
                             i = 0
                             c = 1
                             while i < parts:
                                 subvideo = video.subclip(start_time, end_time)
                                 bot.send_message(chat_id=chat_id, text='Processando ' + 'parte ' + str(c) + '/' + str(videopart) + '' + load, timeout=60)
-                                subvideo.write_videofile('/home/mateus/Documents/Python/telegram_bot/IGTV/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=24, bitrate='860k')
+                                subvideo.write_videofile(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=30, bitrate='550k')
+                                time.sleep(1)
+                                bot.send_video(chat_id=chat_id, video = open(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', 'rb'), timeout=3600)
                                 c +=1
-                                start_time += 360.00
-                                end_time += 360.00
+                                start_time += 600.00
+                                end_time += 600.00
                                 i += 1
                             if (i == parts):
                                 subvideo = video.subclip(start_time, end_custom)
                                 bot.send_message(chat_id=chat_id, text='Processando ' + 'parte ' + str(c) + '/' + str(videopart) + '' + load, timeout=60)
-                                subvideo.write_videofile('/home/mateus/Documents/Python/telegram_bot/IGTV/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=24, bitrate='860k')
-                            os.remove(path)
-                            igtvPath = glob.glob('/home/mateus/Documents/Python/telegram_bot/IGTV' + '/*.mp4')
+                                subvideo.write_videofile(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', threads=8, preset="veryfast", logger=None, audio_codec='aac', rewrite_audio=False, fps=30, bitrate='550k')
+                                time.sleep(1)
+                                bot.send_video(chat_id=chat_id, video = open(temp_dir + '/IGTV'  + '0' + str(i+1) + '.mp4', 'rb'), timeout=3600)
+                            igtvPath = temp_dir
+                            shutil.rmtree(temp_dir)
                             return igtvPath
             else:
                 decoded = ''
